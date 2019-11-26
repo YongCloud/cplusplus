b8c1b27912737d511fb4f7417f5a070664f5b45e
Xingjian <yang_guang_yong@sina.com>
2019-11-26 16:04:33 +0800

    add card52.cpp

---
+++ card52.cpp
@@ -0,0 +1,110 @@
+/**
+	author: Xingjian
+	since: 2019/11/26
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+using namespace std;
+
+#define CARD 52
+#define NUM 13
+#define COLOR 4
+
+// function declaration
+void init(int *,int);
+void shuffle(int *,int);
+void swap(int *,int *);
+void print(int *,int);
+void print_card(int *,int,const char *[],const char *[]);
+void apply(int *,int,int *[]);
+int main(int argc, char const *argv[])
+{
+	// 4 colors
+	const char *suit[COLOR] = {"Hearts","Diamonds","Clubs","Spades"};
+	// 13 faces
+	const char *face[NUM] = {"Ace","Deuce","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
+	// 52 cards
+	int card[CARD];
+
+	init(card,CARD);
+	shuffle(card,CARD);
+	print(card,CARD);
+	print_card(card,CARD,suit,face);
+
+	int *q[5];
+	apply(card,5,q);
+	cout<<"applied 5 cards:"<<endl;
+	for(int i = 0; i< 5; i++) {
+		int color = *(*(q+i)) / NUM;
+		int num = *(*(q+i)) % NUM;
+		cout<<suit[color]<<"-"<<face[num]<<" ";
+	}
+
+	return 0;
+}
+
+// initialize
+void init(int *p,int n)
+{
+	for(int i = 0; i< n; i++) {
+		*(p+i) = i;
+	}
+}
+
+// shuffle
+void shuffle(int *p,int n)
+{
+	// initialize seed
+	srand(time(NULL));
+	for(int i = 0; i< n; i++) {
+		// generate a random integer between 0 and n
+		int r = rand()%n;
+		swap(p+i,p+r);
+	}
+}
+
+// swap two integer
+void swap(int *a,int *b)
+{
+	*a = *a ^ *b;
+	*b = *a ^ *b;
+	*a = *a ^ *b;
+}
+
+// apply card
+void apply(int *p,int n,int *q[])
+{
+	for(int i = 0; i< n; i++) {
+		*(q+i) = p+i;
+	}
+}
+
+// print array element
+void print(int *p,int n)
+{
+	for(int i = 0; i< n; i++) {
+		cout<<setw(3)<<*(p+i)<<" ";
+		if((i+1) % NUM == 0) {
+			cout<<endl;
+		}
+	}
+	cout<<endl;
+}
+
+// print array card
+void print_card(int *p,int n,const char *suit[],const char *face[])
+{
+	for(int i = 0; i<n; i++) {
+		int color = *(p+i) / NUM;
+		int num = *(p+i) % NUM;
+		cout<<setw(7)<<suit[color]<<"-"<<face[num]<<" ";
+		if((i+1) % 4 == 0) {
+			cout<<endl;
+		}
+	}
+	cout<<endl;
+}
+
