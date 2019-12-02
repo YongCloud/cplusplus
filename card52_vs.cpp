50cbc50f46f6850ab0e52a4e6861e5f286abb9e7
xingjian <yang_guang_yong@sina.com>
2019-12-02 22:33:12 +0800

    2019-12-02 teach log

---
+++ card52_vs.cpp
@@ -0,0 +1,168 @@
+/*
+ version with VS compiler, terrible compiler!
+*/
+#include "StdAfx.h"
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+using namespace std;
+
+#define CARD 52
+#define NUM 13
+#define COLOR 4
+
+void init(int *,int);
+void shuffle(int *,int);
+namespace my {
+	void swap(int *,int *);
+}
+void print(int *,int);
+void print_card(int *,int,const char *[],const char *[]);
+void apply(int *,int,int *[]);
+void print_apply(int *[],int,const char *[],const char *[]);
+bool have_one_pairs(int *[],int);
+int pairs(int *[],int);
+
+int main(int argc,char const *argv[])
+{
+	const char *suit[COLOR]= {"Hearts","Diamonds","Clubs","Spades"};
+	const char *face[NUM]= {"Ace","Deuce","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
+	int card[CARD];
+
+	init(card,CARD);
+	shuffle(card,CARD);
+	cout<<"shuffled card index:"<<endl;
+	print(card,CARD);
+	cout<<"shuffled card:"<<endl;
+	print_card(card,CARD,suit,face);
+
+
+	int *q[5];
+	apply(card,5,q);
+	print_apply(q,5,suit,face);
+	cout<<"these cards have one pairs? ";
+	if(have_one_pairs(q,5)) {
+		cout<<"YES"<<endl;
+	} else {
+		cout<<"NO"<<endl;
+	}
+
+
+	return 0;
+}
+
+void init(int *p,int n)
+{
+	for(int i = 0; i< n; i++) {
+		*(p+i) = i;
+	}
+}
+
+
+// shuffle
+void shuffle(int *p,int n)
+{
+	// initialize random generator seed
+
+	for(int i = 0; i< n; i++) {
+		// generate a random integer between 0 and n
+		int r = rand()%n;
+		my::swap(p+i,p+r);
+	}
+}
+
+// swap two integer
+void my::swap(int *a,int *b)
+{
+	// take care of a=b, otherwise *a will be 0
+	if(a == b) {
+		return;
+	}
+	*a = *a ^ *b;
+	*b = *a ^ *b;
+	*a = *a ^ *b;
+}
+
+
+// apply card
+void apply(int *p,int n,int *q[])
+{
+	for(int i = 0; i< n; i++) {
+		*(q+i) = p+i;
+	}
+}
+
+
+// print array element
+void print(int *p,int n)
+{
+	for(int i = 0; i< n; i++) {
+		cout<<setw(3)<<*(p+i)<<" ";
+		if((i+1) % NUM == 0) {
+			cout<<endl;
+		}
+	}
+	cout<<endl;
+}
+
+
+// print array card
+void print_card(int *p,int n,const char *suit[],const char *face[])
+{
+	for(int i = 0; i<n; i++) {
+		int color = *(p+i) / NUM;
+		int num = *(p+i) % NUM;
+		cout<<setw(7)<<suit[color]<<"-"<<face[num]<<" ";
+		if((i+1) % 4 == 0) {
+			cout<<endl;
+		}
+	}
+	cout<<endl;
+}
+
+
+// print applied card
+void print_apply(int *q[],int n,const char *suit[],const char *face[])
+{
+	cout<<"applied "<<n<<" cards:"<<endl;
+	for(int i = 0; i< n; i++) {
+		/*
+		int color = *q[i] / NUM;
+		q[i] = *(q+i)
+		*/
+		int color = *(*(q+i)) / NUM;
+		int num = *(*(q+i)) % NUM;
+		cout<<suit[color]<<"-"<<face[num]<<" ";
+	}
+	cout<<endl;
+}
+
+
+// these cards have one pairs?
+bool have_one_pairs(int *q[],int n)
+{
+	int pair = pairs(q,n);
+	if(1 == pair) {
+		return true;
+	}
+	return false;
+}
+
+
+// get pairs int the card
+int pairs(int *q[],int n)
+{
+	int pair = 0;
+	for(int i=0; i<n; i++) {
+		int the = *(*(q+i)) % NUM;
+		for(int j=i+1; j<n; j++) {
+			int that = *(*(q+j)) % NUM;
+			if(the == that) {
+				pair++;
+			}
+		}
+	}
+	return pair;
+}
---
+++ e_x.cpp
@@ -0,0 +1,72 @@
+/*
+ calculate e to the power of x.
+ author: Xingjian
+ since: 2019/12/02
+*/
+#include <iostream>
+using namespace std;
+
+// constant
+#define THRESH_HOLD 1.0e-5
+
+// function declaration
+double e_x(int);
+double item(int,int);
+int power(int,int);
+int factor(int);
+
+int main()
+{
+	int x;
+	//printf("%lf\n",THRESH_HOLD);
+	cout<<"please input a integer:"<<endl;
+	cin>>x;
+	cout<<"result: "<<e_x(x)<<endl;
+	return 0;
+}
+
+// return e to the power of x
+double e_x(int x)
+{
+	double sum = 0.0;
+	int i = 0;
+	double entry = item(x,i);
+	while(entry >= THRESH_HOLD) {
+		sum += entry;
+
+		i++;
+		entry = item(x,i);
+	}
+	return sum;
+}
+
+// return power(x,n) / factor(n)
+double item(int x,int n)
+{
+	return (double)power(x,n) / factor(n);
+}
+
+// return power(x,n)
+int power(int x,int n)
+{
+	int r = 1;
+	for(int i = 1; i <= n; i++) {
+		r *= x;
+	}
+	return r;
+}
+
+// return factor of n
+int factor(int n)
+{
+	if(0 == n) {
+		return 1;
+	}
+	int r = 1;
+	for(int i = 1; i <= n; i++) {
+		// r = r * i;
+		r *= i;
+	}
+	return r;
+}
+
