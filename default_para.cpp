c089ac411ed4f87ff27a356e6e9cc804bb6c0027
xingjian <yang_guang_yong@sina.com>
2019-12-11 08:10:59 +0800

    fix bug

--- default_para.cpp
+++ default_para.cpp
@@ -5,15 +5,16 @@
 */
 #include <iostream>
 using namespace std;
- 
+
 // function declaration
 int sum(int,int b = 20);
 
-int main(){
+int main() {
 	cout<<sum(10)<<endl;
+	cout<<sum(10,90)<<endl;
 	return 0;
 }
 
-int sum(int a,int b = 20){
+int sum(int a,int b) {
 	return a+b;
-} 
+}
