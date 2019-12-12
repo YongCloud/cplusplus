7f2fcdcc0f708a771279d2e56b6d55f8b5b65469
Xingjian <yang_guang_yong@sina.com>
2019-12-12 18:34:37 +0800

    Update recursion_demo.cpp

--- recursion_demo.cpp
+++ recursion_demo.cpp
@@ -6,23 +6,23 @@
  f(n)=3*f(n-1) - f(n-2)
  please calculate n where f(n) just more than 7654321.
  so how should we solve this problem?
-
  author xingjian
  since 2019/12/10
 */
 #include <iostream>
+#include <vector>
 using namespace std;
 
 const int thresh_hold = 7654321;
 
 int fun_iter(int);
 int fun_recur(int);
-int fun_dp(int,int *);
+int fun_dp(int);
 
 int main()
 {
 	// pointer to function
-	int (*p)(int) = fun_iter;
+	int (*p)(int) = fun_dp;
 	for(int i = 1; i < 100; i++) {
 		int result = p(i);
 		if(result > thresh_hold) {
@@ -68,61 +68,22 @@ int fun_recur(int n)
 /*
   solve with recursion and dynamic programming
 */
-class ArrayList
-{
-	private:
-		int *arr;
-		int position;
-		int size;
-
-	public:
-		ArrayList(int size) {
-			cout<<"constructor called"<<endl;
-			arr = new int[size];
-			if(arr == NULL) {
-				cout<<"Failed to apply for memory."<<endl;
-			} else {
-				this->size = size;
-				position = 0;
-			}
-		}
-
-		~ArrayList() {
-			cout<<"destructor called"<<endl;
-			delete []arr;
-			arr = NULL;
-		}
-
-		bool add(int e) {
-			if(position >= size) {
-				cout<<"resize array"<<endl;
-				int *p = arr;
-				arr = new int[size*2];
-				if(arr == NULL) {
-					cout<<"Failed to apply for memory."<<endl;
-					arr = p;
-				} else {
-					size = 2 * size;
-					for(int i = 0; i<position; i++) {
-						*(arr+i) = *(p+i);
-					}
-					*(arr+position) = e;
-					position++;
-					delete []p;
-					p = NULL;
-				}
-			} else {
-				*(arr+position) = e;
-				position++;
-			}
-		}
-
-		int get(int index) {
-
-		}
-};
-
+vector<int> vec;
 int fun_dp(int n)
 {
-
+	// return cached value
+	if(n <= vec.size()) {
+		return vec[n-1];
+	}
+	// calculate and put result into cache
+	int ret = 0;
+	if(n == 1) {
+		ret = 1;
+	} else if(n == 2) {
+		ret = 4;
+	} else {
+		ret = 3 * fun_dp(n-1) - fun_dp(n-2);
+	}
+	vec.push_back(ret);
+	return ret;
 }
